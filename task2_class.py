a8d4052649578dd8554fc868167b38205bb4724b
R2215228W <167210466+R2215228W@users.noreply.github.com>
2024-05-20 15:04:34 +0200

    task2 part 1
    am done

--- task2_class.py
+++ task2_class.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
+class Point2D:
+    "A class representing a 2D point with x and y coordinates."
+    def __init__(self , x , y):
+        "Initialise the point object with the given x and y coordinates"
+        self.x = x
+        self.y = y
+    def translate(self,dx ,dy):
+        "Translates the point by dx units in x-direction and dy in y-direction"
+        self.x +=dx
+        self.y +=dy
+
+    def __str__(self):
+        "Returns a string representation of the point"
+        return f"({self.x} , {self.y})"
+    def plot(self,color ='green'):
+        "Plots the point on a scatterplot with given color "
+        plt.scatter(self.x , self.y , color=color ,marker="v")
 
